9bedbb8ccb7cf95f0c900ba81aa336d9c86b8a38
Dalwin D. Lewis <lewisdalwin@gmail.com>
2022-06-22 17:12:39 -0600

    added validation code

--- cmd/api/schools.go
+++ cmd/api/schools.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"appletree.dalwinlewis.net/internal/data"
+	"appletree.dalwinlewis.net/internal/validator"
 )
 
 // createSchoolHandler for the "POST /v1/schools" endpoint
@@ -29,6 +30,28 @@ func (app *application) createSchoolHandler(w http.ResponseWriter, r *http.Reque
 		app.badRequestResponse(w, r, err)
 		return
 	}
+
+	// Copy the values from the input struct to a new School struct
+	school := &data.School{
+		Name:    input.Name,
+		Level:   input.Level,
+		Contact: input.Contact,
+		Phone:   input.Phone,
+		Email:   input.Email,
+		Website: input.Website,
+		Address: input.Address,
+		Mode:    input.Mode,
+	}
+
+	// Initialize a new Validator instance
+	v := validator.New()
+
+	// Check the map to determine if there were any validation errors
+	if data.ValidateSchool(v, school); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
 	// Display the request
 	fmt.Fprintf(w, "%+v\n", input)
 }
