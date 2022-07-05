516ed4c638fbef0d7c000ddb264f89d9d7f519cd
Dalwin D. Lewis <lewisdalwin@gmail.com>
2022-07-05 16:42:20 -0600

    updated the updateSchoolHandler() method to handle partial updates

--- cmd/api/schools.go
+++ cmd/api/schools.go
@@ -96,7 +96,7 @@ func (app *application) showSchoolHandler(w http.ResponseWriter, r *http.Request
 }
 
 func (app *application) updateSchoolHandler(w http.ResponseWriter, r *http.Request) {
-	// This method does a complete replacement
+	// This method does a partial replacement
 	// Get the id for the school that needs updating
 	id, err := app.readIDParam(r)
 	if err != nil {
@@ -115,15 +115,18 @@ func (app *application) updateSchoolHandler(w http.ResponseWriter, r *http.Reque
 		}
 		return
 	}
-	// Create an input struct to hold data read in fro mteh client
+	// Create an input struct to hold data read in from the client
+	// We update input struct to use pointers because pointers have a
+	// default value of nil
+	// If a field remains nil then we know that the client did not update it
 	var input struct {
-		Name    string   `json:"name"`
-		Level   string   `json:"level"`
-		Contact string   `json:"contact"`
-		Phone   string   `json:"phone"`
-		Email   string   `json:"email"`
-		Website string   `json:"website"`
-		Address string   `json:"address"`
+		Name    *string  `json:"name"`
+		Level   *string  `json:"level"`
+		Contact *string  `json:"contact"`
+		Phone   *string  `json:"phone"`
+		Email   *string  `json:"email"`
+		Website *string  `json:"website"`
+		Address *string  `json:"address"`
 		Mode    []string `json:"mode"`
 	}
 
@@ -133,17 +136,32 @@ func (app *application) updateSchoolHandler(w http.ResponseWriter, r *http.Reque
 		app.badRequestResponse(w, r, err)
 		return
 	}
+	// Check for updates
+	if input.Name != nil {
+		school.Name = *input.Name
+	}
+	if input.Level != nil {
+		school.Level = *input.Level
+	}
+	if input.Contact != nil {
+		school.Contact = *input.Contact
+	}
+	if input.Phone != nil {
+		school.Phone = *input.Phone
+	}
+	if input.Email != nil {
+		school.Email = *input.Email
+	}
+	if input.Website != nil {
+		school.Website = *input.Website
+	}
+	if input.Address != nil {
+		school.Address = *input.Address
+	}
+	if input.Mode != nil {
+		school.Mode = input.Mode
+	}
 
-	// Copy / Update the fields / values in the school variable using the fields
-	// in the input struct
-	school.Name = input.Name
-	school.Level = input.Level
-	school.Contact = input.Contact
-	school.Phone = input.Phone
-	school.Email = input.Email
-	school.Website = input.Website
-	school.Address = input.Address
-	school.Mode = input.Mode
 	// Perform validation on the updated School. If validation fails, then
 	// we send a 422 - Unprocessable Entity respose to the client
 	// Initialize a new Validator instance
@@ -192,4 +210,5 @@ func (app *application) deleteSchoolHandler(w http.ResponseWriter, r *http.Reque
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
+
 }
